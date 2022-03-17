70a72cd77adc0b5a854fd954c12e75c44155def1
Relyg15 <98603697+Relyg15@users.noreply.github.com>
2022-03-17 15:25:25 -0400

    Add files via upload

---
+++ PHD.java
@@ -0,0 +1,17 @@
+public class PHD {
+    private String name;
+    private int gb;
+    public PHD(String n, int giga){
+        name = n;
+        gb = giga;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public int getGb() {
+        return gb;
+    }
+}
---
+++ PV.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class PV {
+    private PHD info;
+    private static String pd [][];
+    public PV(String name, String PHDName, int ){
+        hardDriveName = name;
+    }
+
+        public static void generateUUID(){
+            UUID u = UUID.randomUUID();
+        }
+
+    public String getHardDriveName() {
+        return hardDriveName;
+    }
+
+    public String[][] getPd() {
+
+        return pd;
+    }
+}
